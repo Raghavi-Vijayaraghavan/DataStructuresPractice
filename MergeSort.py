4dd38881e06695dfb8bae186b3ec9f4c7a43e973
Raghavi-Vijayaraghavan <v.raaghavi@gmail.com>
2024-06-10 18:51:43 -0500

    Add files via upload

--- MergeSort.py
+++ MergeSort.py
@@ -1,1 +1,27 @@
-def mergeSort(testList):
+def msDivide(a):
+    if len(a) <= 1:
+        return a
+    else:
+        mid = len(a)//2
+        lHalf = a[:mid]
+        rHalf = a[mid:]
+        leftElements = msDivide(lHalf)
+        rightElements = msDivide(rHalf)
+        return msMerge(leftElements, rightElements)
+
+def msMerge(l,r):
+    res = []
+    i = j = 0
+    while i < len(l) and j < len(r):
+        if l[i] <= r[j]:
+            res.append(l[i])
+            i += 1
+        else:
+            res.append(r[j])
+            j += 1
+
+    res.extend(l[i:])
+    res.extend(r[j:])
+    return res
+
+print(msDivide([5,4,3]))
